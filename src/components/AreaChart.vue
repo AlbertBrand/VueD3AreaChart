c3dbc948b2b8186c7ff9f22fc0557d2e52088d86
Frank van Wijk <fvanwijk@xebia.com>
2019-12-08 15:03:24 +0100

    Render static chart based on data and using D3 shape helpers

--- src/components/AreaChart.vue
+++ src/components/AreaChart.vue
@@ -1,12 +1,15 @@
 <template>
-  <div>
-    Scale X: {{ scaleX(0) }}<br />
-    Scale Y: {{ scaleY(0) }}
-  </div>
+  <svg :width="width" :height="height">
+    <g>
+      <path class="area" :d="areaPath(this.data)" />
+      <path class="line" :d="linePath(this.data)" />
+    </g>
+  </svg>
 </template>
 
 <script>
 import { scaleLinear } from "d3-scale";
+import { area, line } from "d3-shape";
 
 const MARGIN = 25;
 
@@ -36,9 +39,29 @@ export default {
       return scaleLinear()
         .domain([0, this.data.length ? Math.max.apply(this, this.data) : 0])
         .range([this.height - MARGIN, MARGIN]);
+    },
+    areaPath() {
+      return area()
+        .x((d, i) => this.scaleX(i))
+        .y0(this.scaleY(0))
+        .y1(d => this.scaleY(d));
+    },
+    linePath() {
+      return line()
+        .x((d, i) => this.scaleX(i))
+        .y(d => this.scaleY(d));
     }
   }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.area {
+  fill: #9d92b2;
+}
+.line {
+  stroke: #6c1d5f;
+  stroke-width: 3px;
+  fill: none;
+}
+</style>
