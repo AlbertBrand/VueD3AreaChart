6f6ae1a1c05541861cfc3c50a7b3c2a98772c712
Frank van Wijk <fvanwijk@xebia.com>
2019-12-08 14:26:08 +0100

    Add new empty Demo component
    HelloWorld component is removed.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <demo />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Demo from "./components/Demo.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    Demo
   }
 };
 </script>
---
+++ src/components/Demo.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Chart placeholder</div>
+</template>
+
+<script>
+export default {
+  name: "Demo"
+};
+</script>
+
+<style scoped></style>
