a19ed5a0666d4e7e5d5f07bdf0c3fa22406623d7
Frank van Wijk <fvanwijk@xebia.com>
2021-06-15 15:33:46 +0200

    Render static chart based on data and using D3 shape helpers

--- vue3-d3-example/src/components/AreaChart.vue
+++ vue3-d3-example/src/components/AreaChart.vue
@@ -1,13 +1,16 @@
 <template>
-  <div>
-    Scale X: {{ scaleX(0) }}<br />
-    Scale Y: {{ scaleY(0) }}
-  </div>
+  <svg :width="width" :height="height">
+    <g>
+      <path class="area" :d="areaPath" />
+      <path class="line" :d="linePath" />
+    </g>
+  </svg>
 </template>
 
 <script lang="ts">
 import { scaleLinear } from 'd3-scale';
 import { max } from 'd3-array';
+import { area, line } from 'd3-shape';
 import { computed, defineComponent, PropType } from 'vue';
 
 const MARGIN = 25;
@@ -40,7 +43,25 @@ export default defineComponent({
         .range([props.height - MARGIN, MARGIN]);
     });
 
+    const areaPathGenerator = computed(() => {
+      return area<number>()
+        .x((d, i) => scaleX.value(i))
+        .y0(scaleY.value(0))
+        .y1((d) => scaleY.value(d));
+    });
+
+    const linePathGenerator = computed(() => {
+      return line<number>()
+        .x((d, i) => scaleX.value(i))
+        .y((d) => scaleY.value(d));
+    });
+
+    const areaPath = computed(() => areaPathGenerator.value(props.chartData) ?? '');
+    const linePath = computed(() => linePathGenerator.value(props.chartData) ?? '');
+
     return {
+      areaPath,
+      linePath,
       scaleX,
       scaleY,
     };
@@ -48,4 +69,13 @@ export default defineComponent({
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.area {
+  fill: #9d92b2;
+}
+.line {
+  stroke: #6c1d5f;
+  stroke-width: 3px;
+  fill: none;
+}
+</style>
