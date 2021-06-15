a8713b8bef4782c3d54b37e037de64880e703952
Frank van Wijk <fvanwijk@xebia.com>
2021-06-15 16:26:04 +0200

    Generate random data for chart every 2 seconds

--- vue3-d3-example/src/components/AreaChartDemo.vue
+++ vue3-d3-example/src/components/AreaChartDemo.vue
@@ -4,15 +4,29 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { interval, now } from 'd3-timer';
+import { randomInt } from 'd3-random';
+import { defineComponent, onMounted, ref } from 'vue';
 import AreaChart from './AreaChart.vue';
+
+const REFRESH_RATE = 2000;
 
 export default defineComponent({
   components: {
     AreaChart,
   },
   setup() {
-    const data = ref<number[]>([1, 5, 3, 10, 8]);
+    const data = ref<number[]>([]);
+
+    onMounted(() => {
+      interval(
+        () => {
+          data.value = Array.from({ length: 10 }, randomInt(0, 11));
+        },
+        REFRESH_RATE,
+        now() - REFRESH_RATE
+      );
+    });
 
     return { data };
   },
